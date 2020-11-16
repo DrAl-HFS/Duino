d31b8151203afdd31e543713e296bd324d78c5c0
DrAl-HFS <DrAl-HFS@github.com>
2020-11-16 11:56:07 +0000

    +Performance checks, reorganising..

--- AD9833/AD9833.ino
+++ AD9833/AD9833.ino
@@ -14,10 +14,10 @@
 
 /***/
 
+#include "DA_ad9833HW.hpp"
 #include "Common/DA_StrmCmd.hpp"
-#include "Common/DA_ad9833.hpp"
 #include "Common/DA_Timing.hpp"
-#include "Common/DA_Counting.hpp"
+//#include "Common/DA_Counting.hpp"
 
 StreamCmd gStreamCmd;
 DA_AD9833Control gSigGen;
@@ -27,28 +27,11 @@ CClock gClock(AVR_CLOCK_TRIM);
 // SIGNAL blocks other ISRs - check use of sleep()
 #ifdef AVR_CLOCK_TN
 
-#ifdef AVR_CLOCK_OFLO // vs. OCMP
-
-#if (2 == AVR_CLOCK_TN)
-SIGNAL(TIMER2_OVF_vect) { gClock.nextIvl(); }
-#endif // (0 == AVR_TN)
-#if (0 == AVR_CLOCK_TN)
-SIGNAL(TIMER0_OVF_vect) { gClock.nextIvl(); }
-#endif // (0 == AVR_TN)
-
-#if (1 == AVR_CLOCK_TN)
-SIGNAL(TIMER1_OVF_vect) { gClock.nextIvl(); }
-#endif // (1 == AVR_TN)
-
-#else // OFLO vs. OCMP
-
 #if (2 == AVR_CLOCK_TN)
 SIGNAL(TIMER2_COMPA_vect) { gClock.nextIvl(); }
 //SIGNAL(TIMER2_OCA_vect) { gClock.nextIvl(); }
 //SIGNAL(TIMER2_OCB_vect) { gClock.nextIvl(); }
 #endif
-
-#endif // OFLO vs. OCMP
 
 #endif // AVR_CLOCK_TN
 
@@ -100,58 +83,64 @@ void setup (void)
    //Serial.println(r);
    interrupts();//SEI;
    //cli();
+   gClock.intervalStart();
 } // setup
 
-void sysLog (uint8_t d)
+void sysLog (Stream& s, uint8_t events)
 {
-static uint8_t msP=-1;
-   uint8_t msBCD[3];
-   int8_t n;
-   char str[32];
+  uint8_t msBCD[3];
+  char str[32];
+  int8_t m=sizeof(str)-1, n=0;
   
-   if (d > 0)
-   {
-      convMilliBCD(msBCD, 1, gClock.tick);
+  convMilliBCD(msBCD, 1, gClock.tick);
+  { // DEPRECATE
+static uint8_t msP=-1;
+    if (((0x0F & msBCD[0]) == (0x0F & msP))) return;
+    msP= msBCD[0];
+  }
 
-      if (((0xF0 & msBCD[0]) != (0xF0 & msP)))
-      {
-         msP= msBCD[0];
-         n= gClock.getStrHM(str, sizeof(str)-1, ':');
-         n+= hex2ChU8(str+n, msBCD[0]); // seconds
+  str[n++]= 'V';
+  n+= hex2ChU8(str+n, events);
+  str[n++]= ' ';
+  n+= gClock.getStrHM(str+n, m-n, ':');
+  n+= hex2ChU8(str+n, msBCD[0]); // seconds
 #if 0
-         str[n++]= '.';
-         n+= hex2ChU8(str+n, msBCD[1]); // centi-sec
+  str[n++]= '.';
+  n+= hex2ChU8(str+n, msBCD[1]); // centi-sec
 #endif
-         //n+= snprintf(str+n, sizeof(str)-n, " %uHz", gSigGen.getF());
+  n+= snprintf(str+n, m-n, " S%u,B%u,%04X", gSigGen.reg.dbgTransClk, gSigGen.reg.dbgTransBytes, gSigGen.reg.last);
+  gSigGen.reg.dbgTransClk= gSigGen.reg.dbgTransBytes= 0;
+  //n+= snprintf(str+n, m-n, " %uHz", gSigGen.getF());
 #ifdef DA_COUNTING_H
-         n+= snprintf(str+n, sizeof(str)-n, " C=%u ", gCount.c);
+  n+= snprintf(str+n, m-n, " C=%u ", gCount.c);
 #else
-         str[n++]= ' ';
-         str[n]= 0;
+  str[n++]= ' ';
+  str[n]= 0;
 #endif
-         Serial.print(str);
-         gSigGen.changeMon(true);
-      }
-   }
+  s.print(str);
+  gSigGen.changeMon(true);
 } // sysLog
 
 CmdSeg cmd; // Would be temp on stack but problems arise...
 
 void loop (void)
 {
-  uint8_t d= gClock.update();
-  if (d > 0)
+  uint8_t ev= gClock.update();
+  if (ev > 0)
   { // <=1KHz update rate
     if (gSigGen.resetClear()) { Serial.println("-RST"); } // Previously started reset completes
     if (gStreamCmd.read(cmd,Serial))
-    { 
+    {
+      ev|= 0x80;
       gSigGen.apply(cmd);
     }
     if (gSigGen.resetPending()) { Serial.println("+RST"); } // Reset begins (completes next cycle) 
-    else { gSigGen.sweepStep(d); }
+    else { gSigGen.sweepStep(ev&0xF); }
     
-    sysLog(d);
+    if (gClock.intervalUpdate()) { ev|= 0x40; }
+    if (ev & 0xF0) { sysLog(Serial,ev); }
     gSigGen.commit(); // send whatever needs sent
+    ev= 0;
   }
 } // loop
 
