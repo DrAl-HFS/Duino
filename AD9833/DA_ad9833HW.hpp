e076d32c2467e845709ad26d015487939fe73b71
DrAl-HFS <DrAl-HFS@github.com>
2020-11-17 12:39:19 +0000

    Accelerated SPI transfer

--- AD9833/DA_ad9833HW.hpp
+++ AD9833/DA_ad9833HW.hpp
@@ -21,7 +21,11 @@
 
 // Pin number used for SPI select (active low)
 // any logic 1|0 output will do...
-#define PIN_SEL 9
+// Just use SS pin - must be an output to prevent
+// SPI HW switching to slave mode (master + slave
+// operation requires some extra signalling/arbitration)
+#define PIN_SEL SS // (#10 on Uno)
+// 9
 // Hardware SPI pins used implicitly
 //#define PIN_SCK SCK   // (#13 on Uno)
 //#define PIN_DAT MOSI  // (#11 on Uno)
@@ -68,6 +72,24 @@ public:
 
 #include "Common/DA_Timing.hpp"
 
+//uint16_t rd16BE (const uint8_t b[2]) { return((256*(uint16_t)(b[0])) | b[1]); }
+
+#if (SS == PIN_SEL) // Directly toggle SS=PB2 for performance
+#define SET_SEL_LO() PORTB &= ~(1<<2) // should generate CBI / SBI 
+#define SET_SEL_HI() PORTB |= (1<<2)  // instructions as below
+/* ...arduino.../hardware/tools/avr/avr/include/avr/iom328p.h
+#define SET_SEL_LO() { asm("CBI 0x25, 2"); }
+#define SET_SEL_HI() { asm("SBI 0x25, 2"); }
+TODO - properly comprehend gcc assembler arg handling...
+#define ASM_CBI(port,bit) { asm("CBI %0, %1" : "=r" (port) : "0" (u)); }
+#define SET_SEL_LO() { asm("CBI %0, 2" : "=" PORTB ); } 
+#define SET_SEL_HI() { asm("SBI %0, 2" : "=" PORTB ); }
+*/
+#else   // portable & robust (but slow) versions
+#define SET_SEL_LO() digitalWrite(PIN_SEL, LOW)
+#define SET_SEL_HI() digitalWrite(PIN_SEL, HIGH)
+#endif
+
 class DA_AD9833SPI : public CFastPollTimer
 {
 public:
@@ -75,9 +97,11 @@ public:
 
    DA_AD9833SPI ()
    {
-      pinMode(PIN_SEL, OUTPUT);
-      digitalWrite(PIN_SEL, HIGH);
       SPI.begin();
+#if (SS != PIN_SEL) // SS is already an output
+      pinMode(PIN_SEL, OUTPUT);
+#endif
+      digitalWrite(PIN_SEL, HIGH);
    }
    void write (const uint8_t b[], const uint8_t n)
    {
@@ -85,11 +109,11 @@ public:
       SPI.beginTransaction(SPISettings(8E6, MSBFIRST, SPI_MODE2));
       for (uint8_t i=0; i<n; i+= 2)
       {
-         digitalWrite(PIN_SEL, LOW);   // Falling edge (low level enables input)
+         SET_SEL_LO(); // Falling edge (low level enables input)
+         //SPI.transfer16( rd16BE(b+i) ); // Total waste of time - splits into bytes internally
          SPI.transfer(b[i+1]);  // MSB (send in big endian byte order)
-         // Presumably SPI routine implements synchronisation here ?
          SPI.transfer(b[i+0]);  // LSB
-         digitalWrite(PIN_SEL, HIGH);  // end shift input (latch to target register)
+         SET_SEL_HI(); // Rising edge latches to target register
       }
       SPI.endTransaction();
       dbgTransClk= diff();
