c726ee3b510914cb55f19290dc14ea9602ada5c1
DrAl-HFS <DrAl-HFS@github.com>
2021-01-28 12:47:03 +0000

    Timing test - curious results...

--- Common/N5/N5_Timing.hpp
+++ Common/N5/N5_Timing.hpp
@@ -13,56 +13,102 @@ class CTimerN5
 {
 public:
    CTimerN5 (void) {;}
-   
-//protected:
+
    void setup (NRF_TIMER_Type *pT=NRF_TIMER2)
-   {		
+   {
       pT->MODE = TIMER_MODE_MODE_Timer;
-      pT->PRESCALER= 4;   // 16 clocks -> micro-second tick
+      pT->PRESCALER= 4;   // 16MHz / 2^4 -> micro-second tick
       pT->TASKS_CLEAR= 1; // clean reset
-      pT->BITMODE= TIMER_BITMODE_BITMODE_16Bit; // 24, 32 unnecessary
+      pT->BITMODE= TIMER_BITMODE_BITMODE_16Bit; // 24, 32 bit comparison unnecessary
       pT->CC[0]= 1000;   // 1ms
-         
+
       // Enable auto clear of count & interrupt for CC[0]
       pT->SHORTS=   TIMER_SHORTS_COMPARE0_CLEAR_Enabled << TIMER_SHORTS_COMPARE0_CLEAR_Pos;
       pT->INTENSET= (TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos);
       // | (TIMER_INTENSET_COMPARE1_Enabled << TIMER_INTENSET_COMPARE1_Pos);
-   } // timerSetup
+   } // setup
 }; // CTimerN5
 
-class CClock : protected CTimerN5
+class CRTClockN5
 {
 public:
-   uint32_t tick;
+   CRTClockN5 (void) { ; }
 
-   CClock (void) { ; } // tick= 0; // default initialisation with zero
-   
-   void init (void) { CTimerN5::setup(); }
-   
+   void setup (NRF_RTC_Type *pT=NRF_RTC0)
+   {
+      pT->TASKS_CLEAR= 1; // clean reset
+      //pT->COUNTER=   0; // CLEAR ???
+      pT->PRESCALER= 2048;   // 32768Hz / 2^11 -> 16Hz
+   } // setup
+}; // CRTClockN5
+
+class CClock : protected CTimerN5, CRTClockN5
+{
+public:
+   uint32_t milliTick; // 49.7 days wrap around
+
+   CClock (void) { ; } // default zero initialisation of all fields
+
+   void init (void) { CTimerN5::setup(); CRTClockN5::setup(); }
+
    void start (void)
    {
       NVIC_EnableIRQ(TIMER2_IRQn);
       NRF_TIMER2->TASKS_START= 1;
+
+      //NVIC_EnableIRQ(RTC0_IRQn);
+      NRF_RTC0->TASKS_START= 1;
    }
+
+   void nextIvl (void) { milliTick++; }
+
+   void printSec (Stream& s, uint32_t sec) const
+   {
+      uint8_t hms[3];
+      char ch[5];
+
+      sec2HMS(hms, sec); // -> 24:60:60
+      // snprintf adds 16KB code! Use BCD->char instead...
+      // snprintf(ch,sizeof(ch)-1,"%02u:%02u:%02u.%03u", hms[0], hms[1], hms[2], ms);
+
+      ch[2]= ':';
+      ch[3]= 0;
+      for (int i=0; i<sizeof(hms); i++)
+      {
+         u8ToBCD4(hms+i, hms[i]); // in-place conversion
+         hex2ChU8(ch, hms[i]);
+         if (i == (sizeof(hms)-1)) { ch[2]= 0; }
+         s.print(ch);
+      }
+   } // printSec
    
-   void nextIvl (void) { tick++; }
+   void printMilliSec (Stream& s, uint16_t ms) const
+   {
+      uint8_t bcd[2];
+      char ch[5];
+      u16ToBCD4(bcd,ms); // hacky reuse...
+      ch[4]= 0;
+      bcd4ToChar(ch, sizeof(ch)-1,bcd,2);
+      ch[0]= '.';
+      s.print(ch);
+   } // printMilliSec
    
    void print (Stream& s) const
    {
-      char b[16];
-      uint32_t r= tick / 1000;
-      uint16_t ms= tick % 1000;
-      uint8_t hms[3];
+      uint32_t sec;
+      uint16_t ms= divmod(sec, milliTick, 1000);
+      printSec(s,sec);
+      printMilliSec(s,ms);
       
-      hms[2]= r % 60;
-      r/= 60;
-      hms[1]= r % 60;
-      r/= 60;
-      hms[0]= r % 24;
-      // r/= 24;
-      snprintf(b,sizeof(b)-1,"%02u:%02u:%02u.%03u", hms[0], hms[1], hms[2], ms); // +16KB !! Flash Img
-      s.print(b);
-   }
+      s.print(" (");
+      sec= NRF_RTC0->COUNTER; // 16Hz
+      ms= ((sec & 0xF) * 625) / 10;
+      sec>>= 4;
+      printSec(s,sec);
+      printMilliSec(s,ms);
+      s.print(")");
+   } // print
+
 }; // CClock
 
 #endif // N5_TIMING
--- UBit/radio/radio.ino
+++ UBit/radio/radio.ino
@@ -4,16 +4,27 @@
 // (c) Project Contributors Jan 2021
 
 #define RADIO_MODE_ESB
-#include <nrf51.h>
-#include <nrf51_bitfields.h>
 #include <RH_NRF51.h>
-//include "MapLED.hpp"
+#include "Common/M0_Util.hpp"
+#include "Common/N5/N5_HWID.hpp"
+#include "Common/N5/N5_Timing.hpp"
 
 #define PIN_BTN_A 5
 #define PIN_BTN_B 11
 
+/*** GLOBALS ***/
+
+CClock gClock;
+
+RH_NRF51 gRF;
+
+int gLogIvl= 100;
+int8_t rmm[2]={127,-128}, gChan=2;
+
+uint32_t gNextTick;
+
+
 /*** ISR ***/
-volatile uint16_t gTick= 0, gTock= 0;
 
 extern "C" {
 // NB - 'duino "magic" name linking requires undecorated symbol
@@ -25,63 +36,26 @@ void TIMER2_IRQHandler (void)
     //{ NRF_TIMER2->INTENCLR= TIMER_INTENSET_COMPARE0_Msk; } // Clear interrupt = single shot
 		NRF_TIMER2->EVENTS_COMPARE[0]= 0; // Clear event (SHORTS resets count)
     while (0 != NRF_TIMER2->EVENTS_COMPARE[0]); // spin sync
-    if (++gTick >= 60000) { ++gTock; gTick-= 60000; }
+    gClock.nextIvl();
   }
 } // IRQ
-
 } // extern "C"
 
-void timerSetup (void)
-{		
-  NRF_TIMER2->MODE = TIMER_MODE_MODE_Timer;
-  NRF_TIMER2->PRESCALER= 4;   // 16 clocks -> micro-second tick
-  NRF_TIMER2->TASKS_CLEAR= 1; // clean reset
-	NRF_TIMER2->BITMODE= TIMER_BITMODE_BITMODE_16Bit; // 24, 32 unnecessary
-	NRF_TIMER2->CC[0]= 1000;   // 1ms
-		
-  // Enable auto clear of count & interrupt for CC[0]
-  NRF_TIMER2->SHORTS=   TIMER_SHORTS_COMPARE0_CLEAR_Enabled << TIMER_SHORTS_COMPARE0_CLEAR_Pos;
-  NRF_TIMER2->INTENSET= (TIMER_INTENSET_COMPARE0_Enabled << TIMER_INTENSET_COMPARE0_Pos);
-  // | (TIMER_INTENSET_COMPARE1_Enabled << TIMER_INTENSET_COMPARE1_Pos);
-  NVIC_EnableIRQ(TIMER2_IRQn);
-} // timerSetup
-
-void timerGo (void) { NRF_TIMER2->TASKS_START= 1; }
-
-// Howto read hardware registers...
-void dump (const NRF_FICR_Type *pF)
-{
-  int n,b;
-  
-  b= pF->CODEPAGESIZE;
-  n= pF->CODESIZE;
-
-  Serial.print("\nubit:P=");
-  Serial.print(b);
-  Serial.print("byte:N=");
-  Serial.println(n);
-} // dump
-
-RH_NRF51 gRF;
+/***/
 
 void setup (void)
 { 
-  timerSetup();
-  Serial.begin(115200);
+  gClock.init();
+  Serial.begin(115200); // ? 230400); //
   
-  dump(NRF_FICR);
+  n5DumpHWID(Serial);
   pinMode(PIN_BTN_A, INPUT);
   pinMode(PIN_BTN_B, INPUT);
   
   gRF.init();
   
-  timerGo();
+  gClock.start();
 } // setup
-
-int gI= 0, gJ= 0;
-int8_t rmm[2]={127,-128}, gChan=2;
-
-volatile uint16_t gNextTick;
 
 void loop (void)
 {
@@ -89,10 +63,11 @@ void loop (void)
   uint8_t n=7;
   b[n]= 0;
   
-  if (gTick >= gNextTick)
+  if (gClock.milliTick >= gNextTick)
   { // much more accurate than delay() but extra +10ms per second ??
     gNextTick+= 10;
-    if (gNextTick >= 60000) { gNextTick-= 60000; }
+    if (--gLogIvl <= 0) { gClock.print(Serial); }
+    //if (gNextTick >= 60000) { gNextTick-= 60000; }
     if (gRF.recv((uint8_t*)b,&n))
     {
       gRF.setModeRx();
@@ -107,17 +82,15 @@ void loop (void)
       r&= 0x7F;
       rmm[0]= min(rmm[0],r);
       rmm[1]= max(rmm[1],r);
-      if (++gI > 100)
-      { // too much for single 10ms slot ???
-        Serial.print(gTock); Serial.print(':'); Serial.print(gTick); Serial.print(' ');
-        Serial.print("C"); Serial.print(gChan); Serial.print(" : ");
-        Serial.print(rmm[0]); Serial.print(','); Serial.println(rmm[1]);// Serial.print(" | ");
-        gI= 0;
-        //if (++gJ > 10) { gJ=0; }
-        gChan= (gChan+1) & 0xF;
-        gRF.setChannel(gChan);
-        rmm[0]= 127; rmm[1]= -128;
-      }
+    }
+    if (gLogIvl <= 0)
+    { // too much for single 10ms slot ???
+      gLogIvl= 100;
+      Serial.print(" CH"); Serial.print(gChan); Serial.print(" : ");
+      Serial.print(rmm[0]); Serial.print(','); Serial.println(rmm[1]);// Serial.print(" | ");
+      gChan= (gChan+1) & 0xF;
+      gRF.setChannel(gChan);
+      rmm[0]= 127; rmm[1]= -128;
     }
   }
   //delay(10);
