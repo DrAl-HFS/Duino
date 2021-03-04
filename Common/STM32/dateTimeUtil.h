3e571440a680ab5bad22e04480b35f86e31884cc
DrAl-HFS <DrAl-HFS@github.com>
2021-03-04 15:07:38 +0000

    Basic test OK

---
+++ Common/STM32/dateTimeUtil.h
@@ -0,0 +1,84 @@
+// Temp hack for testing
+
+#ifndef DTU_H
+#define DTU_H
+
+// max 2 ACII chars -> 2 digit packed bcd.
+// CAVEAT: No range checking!
+uint8_t bcd4FromA (const char a[], int8_t nD)
+{
+   uint8_t r= 0;
+   if (nD > 0)
+   {
+      r= 0xF & (a[0] - '0');
+      if (nD > 1)
+      {
+         r= (r << 4) | (0xF & (a[1] - '0'));
+      }
+   }
+   return(r);
+} // bcd4FromA
+
+// CAVEAT: Ignores any leading non-digit
+uint8_t bcd4FromASafe (const char a[], int8_t nA)
+{
+   if (nA > 0)
+   {
+      uint8_t mD= isdigit(a[0]);
+      if (nA > 1) { mD|= isdigit(a[1])<<1; }
+      switch (mD)
+      {
+         case 0b01 : return bcd4FromA(a+0, 1);
+         case 0b10 : return bcd4FromA(a+1, 1);
+         case 0b11 : return bcd4FromA(a+0, 2);
+      }
+   }
+   return(0);
+} // bcd4FromASafe
+
+int findCh (const char c, const char a[], const int n)
+{
+   int i= 0;
+   while ((i < n) && (a[i] != c)) { ++i; }
+   return(i);
+} // findCh
+
+// Minimal & hacky Julian calendar month text discrimination
+// CAVEAT : GIGO
+int8_t monthNumJulian (const char a[])
+{
+   static const char m1[4]={'J','F','M','A'};
+   static const char m9[4]={'S','O','N','D'};
+   char c= toupper(a[0]);
+   int8_t i;
+   i= findCh(c, m1, sizeof(m1));
+   if (i < sizeof(m1))
+   {  // likely
+      if (1 != i)
+      {
+         c= tolower(a[2]);
+         switch(i)
+         {
+            case 0 :
+               if ('l' == c) { return(7); } // Jul : J*l
+               else if ('u' == tolower(a[1])) { return(6); } // Jun : Ju*
+               break; // Jan : J**
+            case 2 :
+               if ('y' == c) { return(5); } // May
+               break; // Mar : M**
+            case 3 :
+               if ('g' == c) { return(8); } // Aug
+               break; // Apr : A**
+         }
+      }
+      return(1+i); // Jan, Feb, Mar, Apr
+   }
+   else
+   {  // First character only
+      i= findCh(c, m9, sizeof(m9));
+      if (i < sizeof(m9)) { return(9+i); }
+   }
+   return(-1);
+} // monthNumJulian
+
+#endif // DTU_H
