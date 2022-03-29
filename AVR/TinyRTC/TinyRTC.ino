8a76251d00b68eae34b5885993ec932e0027c9eb
DrAl-HFS <DrAl-HFS@github.com>
2022-03-29 19:19:17 +0100

    Factored out AT24C

--- AVR/TinyRTC/TinyRTC.ino
+++ AVR/TinyRTC/TinyRTC.ino
@@ -8,61 +8,7 @@
 
 #include "Common/AVR/DA_Config.hpp"
 #include "Common/CDS1307.hpp"
-
-namespace AT24CHW
-{
-  enum Device : uint8_t { BASE_ADDR=0x50 };
-}; // namespace AT24CHW
-
-#ifndef UU16
-extern "C" { typedef union {uint16_t u16; uint8_t u8[2]; } UU16; }
-#endif
-
-class CAT24C : public CCommonI2CX1
-{
-protected:
-   uint8_t devAddr (void) { return(AT24CHW::BASE_ADDR); }
-   
-   void setAddr (const UU16 a) { writeToRev(devAddr(), a.u8, 2); }
-   
-   void setPageAddr (uint8_t page) { UU16 a; a.u16= page<<5; setAddr(a); }
-   
-public:
-   CAT24C (void) { ; }
-   
-   int readPage (const uint8_t page, uint8_t b[], int n)
-   {
-      setPageAddr(page);
-      return readFrom(devAddr(), b, n);
-   } // readPage
-
-   int writeAddr (const UU16 a, const uint8_t b[], int n)
-   {
-      return(writeToRevThenFwd(devAddr(), a.u8, 2, b, n) - 2);
-   }
-   
-   bool sync (void) { delay(10); return(true); }
-   
-   int writePage (const uint8_t page, const uint8_t b[], int n)
-   {
-      UU16 a={page<<5};
-      return writeAddr(a,b,n);
-   } // readPage
-   
-   void dump (Stream& s, uint8_t page)
-   {
-      uint8_t b[32]; // Wire internal buffer size?
-      uint16_t t[2];
-      int q=sizeof(b), r;
-      t[0]= millis();
-      r= readPage(page, b, sizeof(b));
-      t[1]= millis(); //  
-      s.print("CAT24C::dump() - page,q,r= "); s.print(page); s.print(','); s.print(q); s.print(','); s.print(r); s.println(':');
-      dumpHexTab(s, b, r);
-      s.print("dt="); s.println(t[1]-t[0]); 
-   } // dump
-   
-}; // class CAT24C
+#include "Common/CAT24C.hpp"
 
 CDS1307A gRTC;
 CAT24C gERM;
@@ -79,13 +25,12 @@ void setup (void)
 {
   if (beginSync(DEBUG)) { bootMsg(DEBUG); }
   I2C.begin(); // join i2c bus (address optional for master)
-  //uint8_t b[2]={ DS1307HW::DOW, 5 }; gRTC.writeTo(0x68,b,2);
-  gRTC.testDays(DEBUG,__DATE__);
-  int r= gRTC.setA(__TIME__,NULL,__DATE__);
+  gRTC.dump(DEBUG);
+  //gRTC.testDays(DEBUG,__DATE__);
+  int r= gRTC.setA(__TIME__,NULL,__DATE__,0);
   DEBUG.print("gRTC.setA() -> "); DEBUG.println(r); 
   gRTC.printDate(DEBUG,' ');
   gRTC.printTime(DEBUG);
-  gRTC.dump(DEBUG);
 #if 0
   {
     //const char s1[]="Lorem ipsum dolor sit amet";
@@ -130,6 +75,6 @@ void loop (void)
   int vB= analogRead(A7);
   gRTC.printTime(DEBUG,' '); DEBUG.print(": vB="); DEBUG.println(vB);
   gIter++;
-  gERM.dump(DEBUG, gIter & 0x7); // 0x7F 128*32= 4k
+  //gERM.dump(DEBUG, gIter & 0x7); // 0x7F 128*32= 4k
   delay(1000);
 } // loop
