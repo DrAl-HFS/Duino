0292debf20ce32742794a2d1eccb8ce4f60ae4cc
DrAl-HFS <DrAl-HFS@github.com>
2022-04-01 19:21:40 +0100

    fix

--- AVR/TWISR/TWISR.ino
+++ AVR/TWISR/TWISR.ino
@@ -32,17 +32,23 @@ const char s1[]="* Lorem ipsum dolor sit amet *";
 uint8_t gB[34];
 const uint8_t q= sizeof(gB)-2;
 
-char fillTest (uint16_t a, uint8_t b[])
+void setAddr (uint8_t b[], const uint16_t a)
+{
+  b[0]= a >> 8; 
+  b[1]= a & 0xFF; // form (big endian) page address
+} // setAddr
+
+char fillTest (uint8_t b[])
 {
   uint8_t t[4];
   
-  b[0]= t[0]= a >> 8; b[1]= t[1]= a & 0xFF; // form (big endian) page address
+  t[0]= b[0]; t[1]= b[1];
   gTWI.write(0x50,t,2); // set page
   gTWI.read(0x50,t+2,2); // retrieve 2 Bytes
   gTWI.sync(); //delay(50); 
   if ((t[0] != t[3]) || (t[1] != t[2])) // Check for stored page address match
   {
-    b[2]= a & 0xFF; b[3]= a >> 8; // page begins with its own (little endian) address
+    b[2]= b[1]; b[3]= b[0]; // page begins with its own (little endian) address
     //for (int8_t i=0; i<q; i++) { b[i]= q+1-i; }
     if (b[4] != s1[0]) { memcpy(b+4, s1, sizeof(s1)); } // Fill with text as necessary
     gTWI.write(0x50, b, 2+q); // store 32bytes (prefixed with page address)
@@ -53,10 +59,11 @@ char fillTest (uint16_t a, uint8_t b[])
 
 void loop (void)
 {
-  char c= fillTest( ((gIter & 0x7F) << 5), gB); // 4kB -> 128pages of 32Bytes
+  setAddr(gB, (gIter & 0x7F) << 5);
+  char c= fillTest(gB); // 4kB -> 128pages of 32Bytes
   DEBUG.println(c);
   gTWI.sync(); // delay(50);
-  gTWI.write(0x50,gB,2); // reset page - address has been set by fillTest()
+  gTWI.write(0x50,gB,2); // reset page
   gTWI.read(0x50,gB+2,q); // retrieve 32 Bytes
   gTWI.sync(); // delay(50);
   dumpHexTab(DEBUG, gB+2, q);
--- Common/AVR/DA_TWISR.hpp
+++ Common/AVR/DA_TWISR.hpp
@@ -1,6 +1,6 @@
 // Duino/Common/AVR/DA_TWISR.hpp - AVR two wire (I2C-like) interrupt driven communication, C++ class.
 // Adapted from C-code authored by "kkempeneers" (as "twi.c" - without copyright or licence declaration)
-// obtained from:- 
+// obtained from:-
 // https://www.avrfreaks.net/sites/default/files/project_files/Interrupt_driven_Two_Wire_Interface.zip
 // This approach provides efficient asynchronous communication without buffer copying.
 // This could break in a variety of interesting ways under Arduino, subject to resource requrements.
@@ -32,138 +32,138 @@ protected:
    void start (void) { TWCR= (1<<TWINT)|(1<<TWSTA)|(1<<TWEN)|(1<<TWIE); }
 
    void stop (void) { TWCR|= (1<<TWINT)|(1<<TWSTO); }
-   
-	 void sync (void)
-	 {
-			while(TWCR & (1<<TWSTO));
-	 } // sync
+
+   void sync (void)
+   {
+      while(TWCR & (1<<TWSTO));
+   } // sync
 
 public:
-	 TWIHW (void) { ; }
+   TWIHW (void) { ; }
 
 }; // TWIHW
 
 class TWISR : protected TWIHW
 {
 protected:
-	 volatile uint8_t status, retry_cnt;
+   volatile uint8_t status, retry_cnt;
 
    void start (void)
-   { 
-   	 retry_cnt= 0;
-   	 TWIHW::start();
-     status |= (1<<BUSY);
+   {
+      retry_cnt= 0;
+      TWIHW::start();
+      status |= (1<<BUSY);
    } // start
 
    void stop (void)
    {
-   	  TWIHW::stop();
-		  status&= ~(1<<BUSY);
-	 } // stop
+      TWIHW::stop();
+      status&= ~(1<<BUSY);
+   } // stop
 
 public:
    TWISR (void) : status{0}, retry_cnt{0} { ; }
 
-	 void init (void)
-	 {
+   void init (void)
+   {
 #if defined(TWPS0)
-		  TWSR= 0;
+TWSR= 0;
 #endif
       TWBR= (FOSC / 100000UL - 16)/2; // 100kHz ?
-	 } // init
-	 
-	 bool sync (void)
-	 {
+   } // init
+
+   bool sync (void)
+   {
       while(status & (1<<BUSY)); // Busy wait (or exit with error code)
       return(true);
-	 } // sync
+   } // sync
 
-	 int write (uint8_t address, uint8_t *data, uint8_t bytes)
-	 {
-	    if (sync())
-	    {
-				TWIHW::sync();
-				hwAddr= (address << 1) | TW_WRITE;
-				twi_data= data;
-				twi_bytes= bytes;
-				start();
-				return(bytes);
-			}
-			return 0;
-	 } // write
-
-	 int read (uint8_t address, uint8_t *data, uint8_t bytes)
-	 {
+   int write (uint8_t address, uint8_t *data, uint8_t bytes)
+   {
       if (sync())
       {
-				hwAddr= (address << 1) | TW_READ;
-				twi_data= data;
-				twi_bytes= bytes;
-				start();
-				return(bytes);
-			}
-			return 0;
-	 } // read
-	 
+         TWIHW::sync();
+         hwAddr= (address << 1) | TW_WRITE;
+         twi_data= data;
+         twi_bytes= bytes;
+         start();
+         return(bytes);
+      }
+      return 0;
+   } // write
+
+   int read (uint8_t address, uint8_t *data, uint8_t bytes)
+   {
+      if (sync())
+      {
+         hwAddr= (address << 1) | TW_READ;
+         twi_data= data;
+         twi_bytes= bytes;
+         start();
+         return(bytes);
+      }
+      return 0;
+   } // read
+
    void event (const uint8_t flags)
    {
       switch(flags)
       {
-	       case TW_START :												// Start condition
-	       case TW_REP_START :											// Repeated start condition
-		        if(retry_cnt > 2)
-		        {	// 3 NACKs -> abort
-							  stop();
-			          return;												
-		        }
-		        TWDR= hwAddr; 				// Transmit SLA + Read or Write
-		        TWCR &= ~(1<<TWSTA);	// TWSTA must be cleared by software! This also clears TWINT!!!
-		        break;
+         case TW_START :												// Start condition
+         case TW_REP_START :											// Repeated start condition
+            if(retry_cnt > 2)
+            {	// 3 NACKs -> abort
+                stop();
+                return;
+            }
+            TWDR= hwAddr; 				// Transmit SLA + Read or Write
+            TWCR &= ~(1<<TWSTA);	// TWSTA must be cleared by software! This also clears TWINT!!!
+            break;
 
-				 case TW_MT_SLA_ACK :											// Slave acknowledged address,
-					  retry_cnt= 0;											// so clear retry counter
-					  TWDR= *twi_data;										// Transmit data,
-					  twi_data++;												// increment pointer
-					  TWCR |= (1<<TWINT);									// and clear TWINT to continue
-					  break;
+         case TW_MT_SLA_ACK :											// Slave acknowledged address,
+            retry_cnt= 0;											// so clear retry counter
+            TWDR= *twi_data;										// Transmit data,
+            twi_data++;												// increment pointer
+            TWCR |= (1<<TWINT);									// and clear TWINT to continue
+            break;
 
-				 case TW_MT_SLA_NACK :										// Slave didn't acknowledge address,
-				 case TW_MR_SLA_NACK :
-					  retry_cnt++;											// this may mean that the slave is disconnected
-					  TWCR |= (1<<TWINT)|(1<<TWSTA)|(1<<TWSTO);			// retry 3 times
-					  break;
+         case TW_MT_SLA_NACK :										// Slave didn't acknowledge address,
+         case TW_MR_SLA_NACK :
+            retry_cnt++;											// this may mean that the slave is disconnected
+            TWCR |= (1<<TWINT)|(1<<TWSTA)|(1<<TWSTO);			// retry 3 times
+            break;
 
-	       case TW_MT_DATA_ACK :										// Slave Acknowledged data,
-		        if(--twi_bytes > 0)
-		        {	 // Send more data
-			         TWDR= *twi_data;									// Send it,
-			         twi_data++;											// increment pointer
-			         TWCR |= (1<<TWINT);								// and clear TWINT to continue
-		        }
-						else { stop(); }
-						break;
+         case TW_MT_DATA_ACK :										// Slave Acknowledged data,
+            if(--twi_bytes > 0)
+            {	 // Send more data
+               TWDR= *twi_data;									// Send it,
+               twi_data++;											// increment pointer
+               TWCR |= (1<<TWINT);								// and clear TWINT to continue
+            }
+            else { stop(); }
+            break;
 
          case TW_MT_DATA_NACK :	stop(); break; // Slave didn't acknowledge data
 
          case TW_MT_ARB_LOST : break;				// Single master this can't be!!!
 
-	       case TW_MR_SLA_ACK : // Slave acknowledged address
-		        if(--twi_bytes > 0) { TWCR |= (1<<TWEA)|(1<<TWINT);	} // If there is more than one byte to read acknowledge
-			      else { TWCR |= (1<<TWINT); }							// else do not acknowledge
-		        break;
+         case TW_MR_SLA_ACK : // Slave acknowledged address
+            if(--twi_bytes > 0) { TWCR |= (1<<TWEA)|(1<<TWINT);	} // If there is more than one byte to read acknowledge
+            else { TWCR |= (1<<TWINT); }							// else do not acknowledge
+            break;
 
-	       case TW_MR_DATA_ACK : 										// Master acknowledged data
-		        *twi_data= TWDR;										// Read received data byte
-		        twi_data++;												// Increment pointer
-		        if(--twi_bytes > 0) { TWCR |= (1<<TWEA)|(1<<TWINT);	} // Get next databyte and acknowledge	
-			      else { TWCR &= ~(1<<TWEA);	} // Enable Acknowledge must be cleared by software, this also clears TWINT!!!
-		        break;
+         case TW_MR_DATA_ACK : 										// Master acknowledged data
+            *twi_data= TWDR;										// Read received data byte
+            twi_data++;												// Increment pointer
+            if(--twi_bytes > 0) { TWCR |= (1<<TWEA)|(1<<TWINT);	} // Get next databyte and acknowledge
+            else { TWCR &= ~(1<<TWEA);	} // Enable Acknowledge must be cleared by software, this also clears TWINT!!!
+            break;
 
-				 case TW_MR_DATA_NACK : 	// Master didn't acknowledge data -> end of read process
-		        *twi_data= TWDR; // Read received data byte
-		        stop();
-		        break;
-			} // switch
+         case TW_MR_DATA_NACK : 	// Master didn't acknowledge data -> end of read process
+            *twi_data= TWDR; // Read received data byte
+            stop();
+            break;
+      } // switch
    } // event
 
 }; // class TWISR
